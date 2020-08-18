d60b46ee0f4df0b23ea48e23b36f29ea235c0613
accidentlywoo <accidentlywoo@gmail.com>
2020-08-18 22:29:00 +0900

    chai 사용하기 성공!!!

--- TDD/test/Producer.js
+++
@@ -1,7 +0,0 @@
-
-describe('province', function(){
-    it('shortfall', function(){
-        const asia = new Province(sampleProvinceData());
-        assert.equal(asia.shortfall, 5);
-    });
-});
--- test/Producer.js
+++ test/Producer.js
@@ -1,7 +1,21 @@
 
+const { Province, sampleProvinceData } = require("../Province");
+
+// var assert = require("assert");
+
+const { describe } = require("mocha");
+
 describe('province', function(){
     it('shortfall', function(){
         const asia = new Province(sampleProvinceData());
         assert.equal(asia.shortfall, 5);
     });
 });
+
+const { assert, expect } = require("chai");
+describe('province', () => {
+    it('shortfall', () => {
+        const asia = new Province(sampleProvinceData());
+        expect(asia.shortfall).equals(5);
+    })
+})
