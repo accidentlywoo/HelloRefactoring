3cef6d62e0c042384710af3d95a438dca26d342a
accidentlywoo <accidentlywoo@gmail.com>
2020-08-29 19:55:43 +0900

    출력 로직 함수로 추출

--- chapter6/extractFunc.js
+++ chapter6/extractFunc.js
@@ -1,10 +1,9 @@
 function prinOwing(invoice) {
     printBanner();
     let outstanding = 0;
-    console.log('*************************');
-    console.log('****** 고갹 채무 ********');
-    console.log('*************************');
     
+    printBanner();
+
     // 미해결 채무(outstanding)를 계산한다.
     for(const o of invoice.orders){
         outstanding += o.amout;
@@ -14,8 +13,16 @@ function prinOwing(invoice) {
     const today = Clock.today;
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-    //세부 사항을 출력한다.
-    console.log('고객명 : ${invoice.customer}');
-    console.log('채무액 : ${outstanding}'); 
-    console.log('마감일 : ${invoice.dueDate.toLocaleDateString()}'); 
+    printDetail();
+
+    function printDetail() {
+        console.log('고객명 : ${invoice.customer}');
+        console.log('채무액 : ${outstanding}'); 
+        console.log('마감일 : ${invoice.dueDate.toLocaleDateString()}'); 
+    }
+    function printBanner() {
+        console.log('*************************');
+        console.log('****** 고갹 채무 ********');
+        console.log('*************************');
+    }
 }
