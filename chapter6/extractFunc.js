e5767a91eee440d3e6b4e1fdbfb4031212055eb4
accidentlywoo <accidentlywoo@gmail.com>
2020-08-29 19:47:00 +0900

    chapter 6 오디갓오?

---
+++ chapter6/extractFunc.js
@@ -0,0 +1,8 @@
+function prinOwing(invoice) {
+    printBanner();
+    let outstanding = calculateOutstanding();
+
+    // 세부사항 출력
+    console.log('고객명 : ${invoice.customer}');
+    console.log('채무액 : ${outstanding}');
+}
