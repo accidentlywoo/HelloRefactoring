091b29d55b4ba8518f8f3121e618dc38b49b737d
accidentlywoo <accidentlywoo@gmail.com>
2020-08-29 20:10:28 +0900

    지변수 값을 변경하는 함수

--- chapter6/extractFunc.js
+++ chapter6/extractFunc.js
@@ -7,21 +7,20 @@ function prinOwing(invoice) {
     for(const o of invoice.orders){
         outstanding += o.amout;
     }
-
-    // 마감일(dueDate)을 기록한다.
+    recordDueDate(invoice);
+    printDetail(invoice, outstanding);
+}
+function recordDueDate(invoice) {
     const today = Clock.today;
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
-    printDetail(invoice, outstanding);
-
-    function printDetail(invoice, outstanding) {
-        console.log('고객명 : ${invoice.customer}');
-        console.log('채무액 : ${outstanding}'); 
-        console.log('마감일 : ${invoice.dueDate.toLocaleDateString()}'); 
-    }
-    function printBanner() {
-        console.log('*************************');
-        console.log('****** 고갹 채무 ********');
-        console.log('*************************');
-    }
+}
+function printDetail(invoice, outstanding) {
+    console.log('고객명 : ${invoice.customer}');
+    console.log('채무액 : ${outstanding}'); 
+    console.log('마감일 : ${invoice.dueDate.toLocaleDateString()}'); 
+}
+function printBanner() {
+    console.log('*************************');
+    console.log('****** 고갹 채무 ********');
+    console.log('*************************');
 }
