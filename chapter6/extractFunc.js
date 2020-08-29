dbb5f7572b0af2ea7df521b2f13c6c4fdfb41b6e
accidentlywoo <accidentlywoo@gmail.com>
2020-08-29 19:48:26 +0900

    함수 추출하기

--- chapter6/extractFunc.js
+++ chapter6/extractFunc.js
@@ -2,7 +2,10 @@ function prinOwing(invoice) {
     printBanner();
     let outstanding = calculateOutstanding();
 
-    // 세부사항 출력
-    console.log('고객명 : ${invoice.customer}');
-    console.log('채무액 : ${outstanding}');
+    printDetail(outstanding);
+
+    function printDetail(outstanding) {
+        console.log('고객명 : ${invoice.customer}');
+        console.log('채무액 : ${outstanding}');    
+    }
 }
