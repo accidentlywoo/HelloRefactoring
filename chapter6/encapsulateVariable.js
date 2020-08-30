0b99a1e0c831dabd33afe349e0e770af236712f5
accidentlywoo <accidentlywoo@gmail.com>
2020-08-30 22:53:15 +0900

    나의 테스트는 어디로...

--- chapter6/encapsulateVariable.js
+++
@@ -1,8 +0,0 @@
-let defaultOwner = {firstName: '마틴', lastName: '파울러'};
-
-spaceship.owner = getDefaultOwner();
-
-setDefaultOwner({firstName: '레베카', lastName: '파슨스'});
-
-function getDefaultOwner(){return defaultOwner;}
-function setDefaultOwner(arg){defaultOwner = arg;}
