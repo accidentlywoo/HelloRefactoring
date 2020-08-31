939c13ed5caac033b12476312ea1e37bb3e08238
accidentlywoo <accidentlywoo@gmail.com>
2020-08-31 21:44:17 +0900

    불필요한 변수 제거

--- src/chapter6/splitPhase.js
+++ src/chapter6/splitPhase.js
@@ -1,16 +1,18 @@
 // 상품의 결제 금액을 계산하는 코드
 function priceOrder(product, quantity, shippingMethod) {
+    // const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
+    const priceData = calculatePricingData(product, quantity);
+   return applyShipping(priceData, shippingMethod);
+}
+function calculatePricingData(product, quantity) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
-    const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
-    const price = applyShipping(priceData, shippingMethod);
-    return price;
+    return {basePrice: basePrice, quantity: quantity, discount: discount};
 }
 function applyShipping(priceData, shippingMethod) {
     const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
-    const shippingCost = priceData.quantity * shippingCost;
-    const price = priceData.basePrice - product.discount + shippingMethod;
-    return price;
+    const shippingCost = priceData.quantity * shippingPerCase;
+    return priceData.basePrice - product.discount + shippingMethod;
 }
