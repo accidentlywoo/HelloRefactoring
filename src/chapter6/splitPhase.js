aa7ecfc4b75a809404d4ec44770433c43c651e73
accidentlywoo <accidentlywoo@gmail.com>
2020-08-31 21:32:04 +0900

    매개변수들을 중간 데이터 구조로 빼기

--- src/chapter6/splitPhase.js
+++ src/chapter6/splitPhase.js
@@ -3,13 +3,14 @@ function priceOrder(product, quantity, shippingMethod) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
-    const priceData = {};
-    const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+    const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
+    const price = applyShipping(priceData, shippingMethod);
     return price;
 }
-function applyShipping(priceData, basePrice, shippingMethod, quantity, discount) {
-    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+function applyShipping(priceData, shippingMethod) {
+    const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
-    const shippingCost = quantity * shippingCost;
+    const shippingCost = priceData.quantity * shippingCost;
+    const price = priceData.basePrice - product.discount + shippingMethod;
     return price;
 }
