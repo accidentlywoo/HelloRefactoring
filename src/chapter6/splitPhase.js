262bafbf99a3e132a4b33b925358510472bc8fa4
accidentlywoo <accidentlywoo@gmail.com>
2020-08-31 21:26:54 +0900

    중간 데이터 만들기

--- src/chapter6/splitPhase.js
+++ src/chapter6/splitPhase.js
@@ -3,14 +3,11 @@ function priceOrder(product, quantity, shippingMethod) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0)
         * product.basePrice * product.discountRate;
-
-    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
-        ? shippingMethod.discountedFee : shippingMethod.feePerCase;
-    const shippingCost = quantity * shippingPerCase;
+    const priceData = {};
     const price = applyShipping(basePrice, shippingMethod, quantity, discount);
     return price;
 }
-function applyShipping(basePrice, shippingMethod, quantity, discount) {
+function applyShipping(priceData, basePrice, shippingMethod, quantity, discount) {
     const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = quantity * shippingCost;
