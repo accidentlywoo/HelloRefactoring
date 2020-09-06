83bb47b328764fe2f3bc245a21dfa6aef65d7b92
accidentlywoo <accidentlywoo@gmail.com>
2020-09-06 17:29:41 +0900

    중첩도가 심한 데이터 구조 쓰기예. 읽기 예

--- src/chapter7/NestEncapsulateRecord.js
+++ src/chapter7/NestEncapsulateRecord.js
@@ -1,5 +1,12 @@
 let customerData = require('./CustomerData.json');
 
 // 쓰기 예
-console.log(customerData[1920]);
+console.log(customerData[1920]);//오예 된다!
+console.log('오예! : ',customerData[1920].usages[2015][1]);
 
+//읽기 예
+function compareUsage (customerID, laterYear, month){
+    const later = customerData[customerID].usages[laterYear][month];
+    const earlier = customerData[customerID].usages[laterYear-1][month];
+    return {laterAmount: later, change: later - earlier};
+}
