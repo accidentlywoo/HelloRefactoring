1ac032313a777fb0aba392b093b3d596388e46b1
accidentlywoo <accidentlywoo@gmail.com>
2020-09-06 16:15:36 +0900

    레코드 객체로 만들기

--- src/chapter7/EncapsulateRecord.js
+++ src/chapter7/EncapsulateRecord.js
@@ -1,9 +1,12 @@
 import {Organization} from "./Organization";
 
-const organization = {name: "애크미 구스베리", country: "GB"};
+// const organization = {name: "애크미 구스베리", country: "GB"};
 
-function getRawDataOfOrganization(){return organization;}
+// function getRawDataOfOrganization(){return organization;}
 
-let result;
-result+= '<h1>${getRawDataOfOrganization().name}</h1>';
-getRawDataOfOrganization().name = newName;
+// let result;
+// result+= '<h1>${getRawDataOfOrganization().name}</h1>';
+// getRawDataOfOrganization().name = newName;
+const organization = new Organization({name: "애크미 구스베리", country: "GB"});
+function getRawDataOfOrganization(){return organization._data;}
+function getOrganization(){return organization;}
