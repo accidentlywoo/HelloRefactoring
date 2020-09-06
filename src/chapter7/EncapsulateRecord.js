b6bcb9f81efebcebdb10cea9fcc699039299ed79
accidentlywoo <accidentlywoo@gmail.com>
2020-09-06 16:23:57 +0900

    필드 펼치기

--- src/chapter7/EncapsulateRecord.js
+++ src/chapter7/EncapsulateRecord.js
@@ -8,8 +8,9 @@ import {Organization} from "./Organization";
 // result+= '<h1>${getRawDataOfOrganization().name}</h1>';
 // getRawDataOfOrganization().name = newName;
 const organization = new Organization({name: "애크미 구스베리", country: "GB"});
-function getRawDataOfOrganization(){return organization._data;}
+// function getRawDataOfOrganization(){return organization._data;}
 function getOrganization(){return organization;}
+
 let newName = 'Woo 뭐야 왜이렇게 쓰는거야';
 getOrganization().name = newName;
 
