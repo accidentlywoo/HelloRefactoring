f26591fb8cd945d094aa7901f430b454994ee003
accidentlywoo <accidentlywoo@gmail.com>
2020-09-06 18:04:59 +0900

    메서드 추출하기

--- src/chapter7/EncapsulateCollection.js
+++ src/chapter7/EncapsulateCollection.js
@@ -3,6 +3,14 @@ class Person{
         this._name = name;
         this._courses = [];
     }
+    addCourse(aCourse){
+        this._courses.push(aCourse);
+    }
+    removeCourse(aCourse, fnIfAbsent = () => {throw new RangeError();}){
+        const index = this._courses.indexOf(aCourse);
+        if(index === -1) fnIfAbsent();// 와씨 뭐야이거
+        else this._courses.splice(index, 1);
+    }
 
     get name(){return this._name;}
     get courses(){return this._courses}
