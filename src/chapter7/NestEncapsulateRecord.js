0dbc31c093098c9d2e262d45ab30515dd8b54817
accidentlywoo <accidentlywoo@gmail.com>
2020-09-06 17:39:27 +0900

    전체 데이터를 표현하는 클래스 정의, 이를 반환하는 함수 만들기

--- src/chapter7/NestEncapsulateRecord.js
+++ src/chapter7/NestEncapsulateRecord.js
@@ -13,5 +13,20 @@ function compareUsage (customerID, laterYear, month){
 // Step 1. 변수 캡슐화
 function getRawDataOfCustomers() {return customerData;}
 function setRawDataOfCustomers(arg){customerData = arg;}
-// 쓰기예
+// 쓰기 예
 getRawDataOfCustomers()[customerID].usages[year][month] = amount;
+// 읽기 예
+function compareUsage(customerID, laterYear, month){
+    const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
+    const earlier = getRawDataOfCustomers()[customerID].usages[laterYear-1][month];
+    return {laterAmount: later, change: later - earlier};
+}
+
+class CustomerData{
+    constructor(data){
+        this._data = data;
+    }
+}
+function getCustomerData(){return customerData;}
+function getRawDataOfCustomers(){return customerData._data;}
+function setRawDataOfCustomers(arg){customerData = new CustomerData(arg);}
