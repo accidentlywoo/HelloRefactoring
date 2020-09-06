f67158fbbbcd9f731d67d55af2cf4a2424b38ff5
accidentlywoo <accidentlywoo@gmail.com>
2020-09-06 17:54:13 +0900

    독립함수 추출

--- src/chapter7/NestEncapsulateRecord.js
+++ src/chapter7/NestEncapsulateRecord.js
@@ -1,28 +1,6 @@
 let customerData = require('./CustomerData.json');
 let lodash =  require('lodash');
 
-// 쓰기 예
-console.log(customerData[1920]);//오예 된다!
-console.log('오예! : ',customerData[1920].usages[2015][1]);
-
-//읽기 예
-function compareUsage (customerID, laterYear, month){
-    const later = customerData[customerID].usages[laterYear][month];
-    const earlier = customerData[customerID].usages[laterYear-1][month];
-    return {laterAmount: later, change: later - earlier};
-}
-// Step 1. 변수 캡슐화
-function getRawDataOfCustomers() {return customerData;}
-function setRawDataOfCustomers(arg){customerData = arg;}
-// 쓰기 예
-getRawDataOfCustomers()[customerID].usages[year][month] = amount;
-// 읽기 예
-function compareUsage(customerID, laterYear, month){
-    const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
-    const earlier = getRawDataOfCustomers()[customerID].usages[laterYear-1][month];
-    return {laterAmount: later, change: later - earlier};
-}
-
 class CustomerData{
     constructor(data){
         this._data = data;
@@ -31,6 +9,9 @@ class CustomerData{
     setUsage(customerID, year, month, amount){
         this._data[customerID].usages[year][month] = amount;
     }
+    usages(customerID, year, month){
+        return this._data[customerID].usages[year][month];
+    }
 }
 function getCustomerData(){return customerData;}
 function getRawDataOfCustomers(){return customerData.rawData;}
@@ -38,3 +19,9 @@ function setRawDataOfCustomers(arg){customerData = new CustomerData(arg);}
 
 // 쓰기 예
 getCustomerData().setUsage(customerID, year, month,amount);
+
+function compareUsage (customerID, laterYear, month){
+    const later = getCustomerData().usages(customerID, laterYear, month);
+    const earlier = getCustomerData().usages(customerID, laterYear-1, month);
+    return {laterAmount: later, change: later - earlier};
+}
