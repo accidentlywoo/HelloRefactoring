afe917263964e1820996facbc89bf0f2365d220d
accidentlywoo <accidentlywoo@gmail.com>
2020-09-09 15:59:24 +0900

    변수 인라인

--- src/chapter7/ReplaceTempWithQuery.js
+++ src/chapter7/ReplaceTempWithQuery.js
@@ -7,10 +7,10 @@ class Order{
         return this._quantity * this._item.price;
     }
     get price(){
-        const basePrice = this.basePrice;
+        // const basePrice = this.basePrice;
         let discountFactor = 0.98;
 
-        if (basePrice > 1000) discountFactor -= 0.03;
+        if (this.basePrice > 1000) discountFactor -= 0.03;
         return basePrice * discountFactor;
     }
 }
