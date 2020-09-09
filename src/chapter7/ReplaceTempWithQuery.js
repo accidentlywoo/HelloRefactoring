b5a8d51a95f175e5cbefa05ca87535bfa8c9033f
accidentlywoo <accidentlywoo@gmail.com>
2020-09-09 15:56:51 +0900

    변수 변경 불가하게 바꾸기

--- src/chapter7/ReplaceTempWithQuery.js
+++ src/chapter7/ReplaceTempWithQuery.js
@@ -4,8 +4,9 @@ class Order{
         this._item = item;
     }
     get price(){
-        let basePrice = this._quantity * this._item.price;
+        const basePrice = this._quantity * this._item.price;
         let discountFactor = 0.98;
+
         if (basePrice > 1000) discountFactor -= 0.03;
         return basePrice * discountFactor;
     }
