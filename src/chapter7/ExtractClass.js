4f2a0bc5a85bff097e2ef44135d297bdae38c497
accidentlywoo <accidentlywoo@gmail.com>
2020-09-09 17:45:03 +0900

    추출한 클래스에 맞는 변수명 변경

--- src/chapter7/ExtractClass.js
+++ src/chapter7/ExtractClass.js
@@ -4,18 +4,29 @@ class Person {
     }
     get name() {return this._name;}
     set name(arg) {return this._name = arg;}
-    get telephoneNumber() {return this._telephoneNumber.telephoneNumber;}
+    get telephoneNumber() {return this._telephoneNumber.toString();}
 
-    get officeAreaCode() {return this._telephoneNumber.officeAreaCode;}
-    set officeAreaCode(arg) {return this._telephoneNumber.officeAreaCode = arg;}
+    get officeAreaCode() {return this._telephoneNumber.areaCode;}
+    set officeAreaCode(arg) {return this._telephoneNumber.areaCode = arg;}
     
-    get officeNumber() {return this._officeNumber;}
-    set officeNumber(arg) {return this._officeNumber = arg;}
+    get officeNumber() {return this._telephoneNumber.number;}
+    set officeNumber(arg) {return this._telephoneNumber.number = arg;}
 }
 
 class TelephoneNumber{
-    get officeAreaCode() {return this._officeAreaCode;}
-    set officeAreaCode(arg) {return this._officeAreaCode = arg;}
+    get areaCode() {return this._areaCode;}
+    set areaCode(arg) {return this._areaCode = arg;}
 
-    get telephoneNumber(){return '(${this.officeAreaCode}) ${this.officeNumber}';}
+    get number() {return this._number;}
+    set number(arg) {return this._number = arg;}
+    get telephoneNumber(){return `(${this.officeAreaCode}) ${this.officeNumber}`;}
+
+    toString() {return `(${this.areaCode}) ${this.number}`;}
 }
+
+let person = new Person();
+person.name = "test";
+person.officeAreaCode = '02'
+person.officeNumber = '2222-2222';
+console.log(person.name);
+console.log(person.telephoneNumber);
