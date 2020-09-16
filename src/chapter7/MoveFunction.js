856921ad93fcea38b537c343214fcd51cab17971
accidentlywoo <accidentlywoo@gmail.com>
2020-09-16 20:27:43 +0900

    총 거리계산 함수 최상위로 복사

--- src/chapter7/MoveFunction.js
+++ src/chapter7/MoveFunction.js
@@ -20,4 +20,10 @@ function trackSummary(points){
     function radians(degrees){}
     function calculateTime(){}
 }
-
+function top_calculateDistance(){
+    let result = 0;
+    for(let i = 1; i < points.length; i++){
+        result += distance(points[i-1], points[i]);
+    }
+    return result;
+}
