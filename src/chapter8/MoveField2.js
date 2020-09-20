0163a7a5e2e17d9c689090a711b772a8ad5c940c
maeve <maeve.woo@goodoc.co.kr>
2020-09-20 18:32:30 +0900

    공유객체로 이동하기

--- src/chapter8/MoveField2.js
+++ src/chapter8/MoveField2.js
@@ -1,15 +1,20 @@
+const { assert } = require("chai");
+
 class Account {
-    constructor(number, type, interestRate){
+    constructor(number, type){
         this._number = number;
         this._type = type;
-        this._interestRate = interestRate;
+        //assert(interestRate === this._type.interestRate);// 로깅이나 해라
     }
 
-    get interestRate(){return this._interestRate;}
+    get interestRate(){return this._type.interestRate;}
 }
 
 class AccountType {
-    constructor(nameString){
+    constructor(nameString, interestRate){
         this._name = nameString;
+        this._interestRate = interestRate;
     }
+    
+    get interestRate(){return this._type.interestRate;}
 }
