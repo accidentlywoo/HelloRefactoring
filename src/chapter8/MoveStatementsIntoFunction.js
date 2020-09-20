d3d6f01c7433ab97ffa2745cec0c6a0071b75161
maeve <maeve.woo@goodoc.co.kr>
2020-09-20 18:46:23 +0900

    문장을 함수로 옮기기

---
+++ src/chapter8/MoveStatementsIntoFunction.js
@@ -0,0 +1,28 @@
+function renderPerson(outStream, person){
+    const result = [];
+    result.push('<p>${person.name}</p>');
+    result.push(renderPhoto(person.photo));
+    result.push('<p>제목 : ${person.photo.title}</p>');
+    result.push(emitPhotoData(person.photo));
+    return result.join("\n");
+}
+
+function renderPhoto(photo){
+    //뭐야 이거 왜없어
+}
+
+function photoDiv(p){
+    return [
+        "div",
+        '<p>제목 : ${p.title}</p>',
+        emitPhotoData(p),
+        "</div>",
+    ].join("\n");
+}
+
+function emitPhotoData(aPhoto){
+    const result = [];
+    result.push('<p>위치 : ${aPhoto.location}</p>');
+    result.push('<p>날찌 : ${aPhoto.date.toDateString()}</p>');
+    return result.join("\n");
+}
