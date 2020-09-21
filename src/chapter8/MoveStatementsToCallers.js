9ee9350de2ebdfe0429ae2184b9be6e820aecf85
maeve <maeve.woo@goodoc.co.kr>
2020-09-21 23:01:48 +0900

    간단한 굳이 묶인 함수를 풀어서 한단계씩 테스트하자

--- src/chapter8/MoveStatementsToCallers.js
+++ src/chapter8/MoveStatementsToCallers.js
@@ -1,7 +1,8 @@
 function renderPerson(outStream, person){
     outStream.write(`<p>${person.name}</p>`);
     renderPhoto(outStream, person.photo);
-    emitPhotoData(outStream, person.photo);
+    zztmp(outStream, person.photo);
+    outStream.write(`<p>위치 : ${person.photo.location}</p>\n`);
 }
 
 function listRecentPhotos(outStream, photos){
@@ -9,14 +10,18 @@ function listRecentPhotos(outStream, photos){
     .filter(p => p.date > recentDateCutoff())
     .forEach(p => {
         outStream.write("<div>\n");
-        emitPhotoData(outStream, p);
+        zztmp(outStream, p);
+        outStream.write(`<p>위치 : ${p.location}</p>\n`);
         outStream.write("</div>\n");
     });
 }
 
 function emitPhotoData(outStream, photo){
+    zztmp(outStream, photo);
     outStream.write(`<p>제목 : ${photo.title}</p>\n`);
+}
+
+function zztmp(){
     outStream.write(`<p>날짜 : ${photo.date.toDateString()}</p>\n`);
     outStream.write(`<p>위치 : ${photo.location}</p>\n`);
-
 }
