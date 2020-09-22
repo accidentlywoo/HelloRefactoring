cd4f3b40d5d4e6398593136f3156f26bc4923e17
maeve <maeve.woo@goodoc.co.kr>
2020-09-22 23:24:30 +0900

    변수를 호출부와 가깝게 배치하기

--- src/chapter8/SlideStatements.js
+++ src/chapter8/SlideStatements.js
@@ -1,12 +1,16 @@
 const pricingPlan = retrievePricingPlan();
-const order = retreiveOrder();
+
 const baseCharge = pricingPlan.base;
 let charge;
 const chargePerUnit = pricingPlan.unit;
+
+const order = retreiveOrder();
 const units = order.units;
-let discount;
+
 charge = baseCharge + units * chargePerUnit;
 let discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0);
+
+let discount;
 discount = discountableUnits * pricingPlan.discountFactor;
 if(order.isRepeat) discount+= 20;
 charge = charge - discount;
