46104a8c709dcef78ca7268aa7e59210e775dbd1
maeve <maeve.woo@goodoc.co.kr>
2020-10-06 22:14:46 +0900

    복제본데이터 활용하기

--- src/chapter8/ReplaceLoopWithPipeline.js
+++ src/chapter8/ReplaceLoopWithPipeline.js
@@ -1,15 +1,15 @@
 function acquireData(input){
     const lines = input.split("\n");
-    let firstLine = true;
+    // let firstLine = true;
     const result = [];
     
     // 반복문에서 사용하는 컬렉션을 가리키는 별도 변수를 새로 만든다.
-    const loopItems = lines;
+    const loopItems = lines.slice(1);
     for(const line of loopItems){
-        if(firstLine){
-            firstLine = false;
-            continue;
-        }
+        // if(firstLine){
+            // firstLine = false;
+            // continue;
+        // }
         if(line.trim() === "") continue;
         const record = line.split(",");
         if(record[1].trim() === "India"){
