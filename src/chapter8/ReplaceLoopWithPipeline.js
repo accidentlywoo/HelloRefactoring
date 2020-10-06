ec4f7576e10427862f111ec9c025ee8042743a3e
maeve <maeve.woo@goodoc.co.kr>
2020-10-06 22:22:06 +0900

    파이프 라인 결과 대입

--- src/chapter8/ReplaceLoopWithPipeline.js
+++ src/chapter8/ReplaceLoopWithPipeline.js
@@ -4,23 +4,23 @@ function acquireData(input){
     const result = [];
     
     // 반복문에서 사용하는 컬렉션을 가리키는 별도 변수를 새로 만든다.
-    const loopItems = lines
+    const result = lines
                     .slice(1)
                     .filter(line => line.trim() != "")
                     .map(line => line.split(","))
                     .filter(record => record[1].trim() === "India")
                     .map(record => ({city: record[0].trim(), phone: record[2].trim()}))
                     ;
-    for(const line of loopItems){
-        // if(firstLine){
-            // firstLine = false;
-            // continue;
-        // }
-        // if(line.trim() === "") continue;
-        const record = line//.split(",");
-        // if(record[1].trim() === "India"){
-            result.push(line)//{city: record[0].trim(), phone: record[2].trim()})
-        // }
-    }
+    // for(const line of loopItems){
+    //     // if(firstLine){
+    //         // firstLine = false;
+    //         // continue;
+    //     // }
+    //     // if(line.trim() === "") continue;
+    //     const record = line//.split(",");
+    //     // if(record[1].trim() === "India"){
+    //         result.push(line)//{city: record[0].trim(), phone: record[2].trim()})
+    //     // }
+    // }
     return result;
 }
