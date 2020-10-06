631fdbb023ee18d1a16fcb9a8ea49b800a68080a
maeve <maeve.woo@goodoc.co.kr>
2020-10-06 22:23:33 +0900

    불필요한 변수사용 없애기

--- src/chapter8/ReplaceLoopWithPipeline.js
+++ src/chapter8/ReplaceLoopWithPipeline.js
@@ -4,13 +4,13 @@ function acquireData(input){
     const result = [];
     
     // 반복문에서 사용하는 컬렉션을 가리키는 별도 변수를 새로 만든다.
-    const result = lines
-                    .slice(1)
-                    .filter(line => line.trim() != "")
-                    .map(line => line.split(","))
-                    .filter(record => record[1].trim() === "India")
-                    .map(record => ({city: record[0].trim(), phone: record[2].trim()}))
-                    ;
+    // const result = lines
+                    // .slice(1)
+                    // .filter(line => line.trim() != "")
+                    // .map(line => line.split(","))
+                    // .filter(record => record[1].trim() === "India")
+                    // .map(record => ({city: record[0].trim(), phone: record[2].trim()}))
+                    // ;
     // for(const line of loopItems){
     //     // if(firstLine){
     //         // firstLine = false;
@@ -22,5 +22,11 @@ function acquireData(input){
     //         result.push(line)//{city: record[0].trim(), phone: record[2].trim()})
     //     // }
     // }
-    return result;
+    return lines
+            .slice(1)
+            .filter(line => line.trim() != "")
+            .map(line => line.split(","))
+            .filter(record => record[1].trim() === "India")
+            .map(record => ({city: record[0].trim(), phone: record[2].trim()}))
+            ;
 }
