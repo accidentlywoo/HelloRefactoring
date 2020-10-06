5f7b549753511e3b524aa2796f38059f6048ea7a
maeve <maeve.woo@goodoc.co.kr>
2020-10-06 22:16:09 +0900

    pipeline의 활용 filter

--- src/chapter8/ReplaceLoopWithPipeline.js
+++ src/chapter8/ReplaceLoopWithPipeline.js
@@ -4,13 +4,15 @@ function acquireData(input){
     const result = [];
     
     // 반복문에서 사용하는 컬렉션을 가리키는 별도 변수를 새로 만든다.
-    const loopItems = lines.slice(1);
+    const loopItems = lines
+                    .slice(1)
+                    .filter(line => line.trim() != "");
     for(const line of loopItems){
         // if(firstLine){
             // firstLine = false;
             // continue;
         // }
-        if(line.trim() === "") continue;
+        // if(line.trim() === "") continue;
         const record = line.split(",");
         if(record[1].trim() === "India"){
             result.push({city: record[0].trim(), phone: record[2].trim()})
