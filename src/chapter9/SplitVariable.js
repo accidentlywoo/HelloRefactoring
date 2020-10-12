3012f248f5b0a6cfe5501b5b19a3d8b3a605adcb
accidentlywoo <accidentlywoo@gmail.com>
2020-10-12 23:40:41 +0900

    용도 분리 대표변수명 선택

--- src/chapter9/SplitVariable.js
+++ src/chapter9/SplitVariable.js
@@ -1,12 +1,14 @@
 function distanceTravelled (scenario, time){
     let result;
-    let acc = scenario.primaryForce / SpeechRecognitionAlternative.mass; // 가속도(a) = 힘(F) / 질량(m)
+    const primaryAcceleration = scenario.primaryForce / scenario.mass;
     let primaryTime = Math.min(time, scenario.delay);
-    result = 0.5 * acc * primaryTime * primaryTime; // 전파된 거리
+
+    result = 0.5 * primaryAcceleration * primaryTime * primaryTime; // 전파된 거리
+
     let secondaryTime = time - scenario.delay;
     if (secendaryTime > 0){ // 두 번째 힘을 반영해 다시 계산
-        let primaryVelocity = acc * scenario.delay;
-        acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;// 변수 두번 대입 -> 역할 두개
+        let primaryVelocity = primaryAcceleration * scenario.delay;
+        let acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;// 변수 두번 대입 -> 역할 두개
         result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
     }
     return result;
