cc4974d4f92e424dca97f3841e6c09343f225c20
maeve <maeve.woo@goodoc.co.kr>
2020-10-14 00:48:21 +0900

    변수 쪼개기

--- src/chapter9/ReplaceDerivedVariableWithQuery.js
+++ src/chapter9/ReplaceDerivedVariableWithQuery.js
@@ -2,12 +2,13 @@ const { expect, assert } = require('chai');
 
 class ProductionPlan{
     constructor(production){
-        this._production = production;
+        this._initialProduction = production;
+        this._productionAccumular = 0;
         this._adjustments = [];
     }
     get production() {
         // assert(this._production == this.calculatedProduction);
-        return this._production;
+        return this._initialProduction + this._productionAccumular;
     }
     applyAdjustment(anAdjustment){
         this._adjustments.push(anAdjustment);
