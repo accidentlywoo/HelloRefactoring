dcfbffae7717dd4b30adfa3ca48793a779ccf265
maeve <maeve.woo@goodoc.co.kr>
2020-10-14 00:44:01 +0900

    예제코드가 뭔 소린지 모르겠지만 메서드 인라인하기, 변수명 변경

--- src/chapter9/ReplaceDerivedVariableWithQuery.js
+++ src/chapter9/ReplaceDerivedVariableWithQuery.js
@@ -3,13 +3,13 @@ const { expect, assert } = require('chai');
 class ProductionPlan{
     get production() {
         // assert(this._production == this.calculatedProduction);
-        return this.calculatedProduction;
+        return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
     }
     get calculatedProduction() {
         return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
     }
     applyAdjustment(anAdjustment){
         this._adjustments.push(anAdjustment);
-        this._production += anAdjustment.amount;
+        // this._production += anAdjustment.amount;
     }
 }
