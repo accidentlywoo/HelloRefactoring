2f5c4d0c0013cbf22a945b741c6e4cbe83a19c7d
maeve <maeve.woo@goodoc.co.kr>
2020-10-13 23:32:46 +0900

    필드명 변경 -> 캡슐화

--- src/chapter9/RenameField.js
+++ src/chapter9/RenameField.js
@@ -2,11 +2,11 @@ const organization = {name: "애크미 구스베리", country: "GB"};
 
 class Organization {
     constructor(data){
-        this._name = data.name;
+        this._title = data.name;
         this._country = data.country;
     }
-    get name() {return this._name;}
-    set name(aString){this._name = aString;}
+    get name() {return this._title;}
+    set name(aString){this._title = aString;}
     get country() {return this._country;}
     set country(aCountryCode){this._country = aCountryCode;}
 }
