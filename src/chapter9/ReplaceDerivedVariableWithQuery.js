1d645cf419c2c59dd3c3bbae104eb0d571970dd2
maeve <maeve.woo@goodoc.co.kr>
2020-10-14 00:50:57 +0900

    메서드 인라인 생략하기

--- src/chapter9/ReplaceDerivedVariableWithQuery.js
+++ src/chapter9/ReplaceDerivedVariableWithQuery.js
@@ -7,8 +7,11 @@ class ProductionPlan{
         this._adjustments = [];
     }
     get production() {
-        // assert(this._production == this.calculatedProduction);
+        assert(this._productionAccumular == this.calculatedProductionAccumulator);
         return this._initialProduction + this._productionAccumular;
+    }
+    get calculatedProductionAccumulator(){
+        return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
     }
     applyAdjustment(anAdjustment){
         this._adjustments.push(anAdjustment);
