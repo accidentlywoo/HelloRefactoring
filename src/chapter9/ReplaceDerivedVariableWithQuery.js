9c987fec358ca4bb7e04b35decc61a2291b1ed5d
maeve <maeve.woo@goodoc.co.kr>
2020-10-14 00:45:49 +0900

    예시 :: 소스가 둘 이상일 때

--- src/chapter9/ReplaceDerivedVariableWithQuery.js
+++ src/chapter9/ReplaceDerivedVariableWithQuery.js
@@ -1,15 +1,16 @@
 const { expect, assert } = require('chai');
 
 class ProductionPlan{
+    constructor(production){
+        this._production = production;
+        this._adjustments = [];
+    }
     get production() {
         // assert(this._production == this.calculatedProduction);
-        return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
-    }
-    get calculatedProduction() {
-        return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
+        return this._production;
     }
     applyAdjustment(anAdjustment){
         this._adjustments.push(anAdjustment);
-        // this._production += anAdjustment.amount;
+        this._production += anAdjustment.amount;
     }
 }
