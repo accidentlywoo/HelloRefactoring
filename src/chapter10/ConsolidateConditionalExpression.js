302ee9c01d96610b887d709a607daec1b37f850c
accidentlywoo <accidentlywoo@gmail.com>
2020-10-18 16:41:14 +0900

    and 사용하기

--- src/chapter10/ConsolidateConditionalExpression.js
+++ src/chapter10/ConsolidateConditionalExpression.js
@@ -7,3 +7,8 @@ function disbilityAmount(anEmployee) {
         (anEmployee.isPartTime));
     }
 }
+
+if(anEmployee.onVacation)
+    if(anEmployee.seniority > 10)
+        return 1;
+    return 0.5;
