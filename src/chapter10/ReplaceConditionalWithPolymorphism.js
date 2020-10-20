e29f101e4899816604ada64522a034b88b002753
maeve <maeve.woo@goodoc.co.kr>
2020-10-20 23:52:09 +0900

    전부 오버라이드하고, 슈퍼 클래스 메소드는 기본 동작만 남긴다

--- src/chapter10/ReplaceConditionalWithPolymorphism.js
+++ src/chapter10/ReplaceConditionalWithPolymorphism.js
@@ -33,24 +33,23 @@ class EuropeanSwallow extends Bird{
         return "보통이다";
     }
 }
-class AfricanSwallow extends Bird{}
-class NorwegianBlueParrot extends Bird{}
+class AfricanSwallow extends Bird{
+    get plumage(){
+        return (bird.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
+    }
+}
+class NorwegianBlueParrot extends Bird{
+    get plumage(){
+        return (bird.voltage > 100) ? "그을렸다" : "예쁘다";
+    }
+}
 
 class Bird{
     constructor(birdObject){
         Object.assign(this, birdObject); //?
     }
     get plumage(){
-        switch (bird.type){
-            case '유럽 제비' :
-                return "보통이다";
-            case '아프리카 제비' :
-                return (bird.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
-            case '노르웨이 파랑 앵무' :
-                return (bird.voltage > 100) ? "그을렸다" : "예쁘다";
-            default :
-                return "알 수 없다";
-        }
+        return "알 수 없다";
     }
     get airSpeedVelocity(){
         switch (bird.type){
