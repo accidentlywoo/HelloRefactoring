767b007f9cb2a09a47bd6e3d4df106aa18aed48b
maeve <maeve.woo@goodoc.co.kr>
2020-10-20 23:57:17 +0900

    비행속도 메소드도 같은 작업을 진행하고, 호출부를 변경하자

--- src/chapter10/ReplaceConditionalWithPolymorphism.js
+++ src/chapter10/ReplaceConditionalWithPolymorphism.js
@@ -2,11 +2,15 @@ const { bind } = require("lodash");
 
 // 새의 종에 따른 비행 속도와 깃털 상태
 function plumages(birds){
-    return new Map(birds.map(b => [b.name, plumage(b)]));
+    return new Map(birds
+                    .map(b => createBird(b))
+                    .map(bird => [bird.name, plumage(bird).plumage]));
 }
 
 function speeds(birds){
-    return new Map(birds.map(b => [b.name, airSpeedVelocity(b)]));
+    return new Map(birds
+                    .map(b => createBird(b))
+                    .map(bird => [bird.name, plumage(bird).airSpeedVelocity]));
 }
 
 function plumage(bird) { // 깃털 상태
@@ -32,15 +36,24 @@ class EuropeanSwallow extends Bird{
     get plumage(){
         return "보통이다";
     }
+    get airSpeedVelocity(){
+        return 35;
+    }
 }
 class AfricanSwallow extends Bird{
     get plumage(){
         return (bird.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
     }
+    get airSpeedVelocity(){
+        return 40 - 2 * bird.numberOfCoconuts;
+    }
 }
 class NorwegianBlueParrot extends Bird{
     get plumage(){
         return (bird.voltage > 100) ? "그을렸다" : "예쁘다";
+    }
+    get airSpeedVelocity(){
+        return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
     }
 }
 
@@ -52,15 +65,6 @@ class Bird{
         return "알 수 없다";
     }
     get airSpeedVelocity(){
-        switch (bird.type){
-            case '유럽 제비' :
-                return 35;
-            case '아프리카 제비' :
-                return 40 - 2 * bird.numberOfCoconuts;
-            case '노르웨이 파랑 앵무' :
-                return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
-            default :
-                return null;
-        }
+        return 35;
     }
 }
