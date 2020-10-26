1f983f8e52f00e2d9c643bbe0450f46020071bee
maeve <maeve.woo@goodoc.co.kr>
2020-10-27 00:29:43 +0900

    두가지 일을 하는 함수를 쪼개기

--- src/chapter10/ReplaceConditionalWithPolymorphism2.js
+++ src/chapter10/ReplaceConditionalWithPolymorphism2.js
@@ -43,10 +43,14 @@ class Rating { // 함수들을 Rating클래스로 묶기.
 
     get voyageAndHistoryLengthFactor(){
         let result = 0;
+        result += this.historyLengthFactor;
         
-        if(history.length > 8) result += 1;
         if(voyage.length > 14) result -= 1;
         return result;
+    }
+    
+    get historyLengthFactor(){
+        return (this.history.length > 8) ? 1 : 0;
     }
 
     get hasChinaHistory(){
@@ -68,10 +72,13 @@ class ExperienceChinaRating extends Rating {
     get voyageAndHistoryLengthFactor(){
         let result = 0;
         result += 3;
-        if (history.length > 10) result += 1;
+        result += this.historyLengthFactor;
         if (voyage.length > 12) result += 1;
         if (voyage.length > 18) result -= 1;
         return result;
+    }
+    get historyLengthFactor(){
+        return (this.history.length > 10) ? 1 : 0 ;
     }
 }
 
