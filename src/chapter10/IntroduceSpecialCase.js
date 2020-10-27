0e45d12e94055d73086e686fea711c8ba8101a7d
accidentlywoo <accidentlywoo@gmail.com>
2020-10-27 23:20:56 +0900

    미확인 고객 처러리 다수 발견

--- src/chapter10/IntroduceSpecialCase.js
+++ src/chapter10/IntroduceSpecialCase.js
@@ -9,3 +9,19 @@ class Customer{
     set billingPlan(arg){...}
     get paymentHistory() {...} // 납부 이력
 }
+
+// 클라이언트 1..
+const aCustomer = site.customer;
+// ... 수많은 코드 ...
+let customerName;
+if (aCustomer === "미확인 고객") customerName = "거주자";
+else customerName = aCustomer.name;
+
+// 클라이언트 2..
+const plan = (aCustomer === "미확인 고객") ? registry.billingPlan.basic : aCustomer.billingPlan;
+
+// 클라이언트 3..
+if (aCustomer !== "미확인 고객") aCustomer.billingPlan = newPlan;
+
+// 클라이언트 4..
+const weeksDelinquent = (aCustomer === "미확인 고객") ? 0 : aCustomer.paymentHistory.weeksDelinquentInLastYear;
