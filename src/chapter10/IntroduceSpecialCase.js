fc101d1ec99c5f563bd07028b5c472951cd3d57e
accidentlywoo <accidentlywoo@gmail.com>
2020-10-28 00:03:34 +0900

    함수 인라인

--- src/chapter10/IntroduceSpecialCase.js
+++ src/chapter10/IntroduceSpecialCase.js
@@ -39,7 +39,7 @@ if (!isUnknown(aCustomer)) aCustomer.billingPlan = newPlan;
 // 클라이언트 4..
 const weeksDelinquent = aCustomer.paymentHistory.weeksDelinquentInLastYear;
 
-const name !isUnknown(aCustomer) ? aCustomer.name : " 미확인 거주자";
+const name = aCustomer.isUnknown ? " 미확인 거주자" : aCustomer.name ;
 
 function isUnknown(arg) {
     if(!((arg instanceof Customer) || (arg instanceof UnknownCustomer)))
