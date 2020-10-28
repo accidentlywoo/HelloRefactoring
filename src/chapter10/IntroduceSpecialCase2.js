113ca64fd6a80f99e46545a527e0d69a98daaf16
maeve <maeve.woo@goodoc.co.kr>
2020-10-28 18:55:54 +0900

    특이 케이스 조건 검사부분 함수 추출:

--- src/chapter10/IntroduceSpecialCase2.js
+++ src/chapter10/IntroduceSpecialCase2.js
@@ -1,4 +1,14 @@
 // 전력이 필요한 현장에 인프라를 설치해 서비스를 제공
+function createUnknownCustomer(){
+    return {
+        isUnknown : true,
+    };
+}
+
+function isUnknown(arg) {
+    return (arg === "미확인 고객");
+}
+
 class Site{
     get customer(){
         return (this._customer === "미확인 고객") ? new UnknownCustomer() : this._customer;
@@ -10,6 +20,7 @@ class Customer{
     get billingPlan() {...} // 요금제
     set billingPlan(arg){...}
     get paymentHistory() {...} // 납부 이력
+    get isUnknown() {return false;}
 }
 
 class NullPaymentHistory {
@@ -20,14 +31,14 @@ class NullPaymentHistory {
 const aCustomer = site.customer;
 // ... 수많은 코드 ...
 let customerName;
-if(aCustomer === "미확인 고객") customerName = "거주자";
+if(isUnknown(aCustomer)) customerName = "거주자";
 else customerName = aCustomer.name;
 
 // 클라이언트 2..
-const plan = (aCustomer === "미확인 고객") ?
+const plan = isUnknown(aCustomer) ?
  registry.billingPlan.basic : aCustomer.billingPlan;
 
 // 클라이언트 3..
-const weeksDelinquent = (aCustomer === "미확인 고객") ?
+const weeksDelinquent = isUnknown(aCustomer) ?
 0 : aCustomer.paymentHistory.weeksDelinquentInLastYear;
 
