541faa29eb27c3089e99fdb33a5917661a796a7d
maeve <maeve.woo@goodoc.co.kr>
2020-10-28 19:35:33 +0900

    미확인 고객 확인 검사로직 함수로 추출

--- src/chapter10/ConversionFunction.js
+++ src/chapter10/ConversionFunction.js
@@ -22,22 +22,29 @@ JSON jsonBoj2 =
     customer: "미확인 고객",
 };
 
+function enrichSite(inputSite){
+    return _.cloneDeep(inputSite);
+}
+
+function isUnknown(aCustomer) {
+    return aCustomer === "미확인 고객";
+}
+
 // 클라이언트 1 
 const rawSite = acquireSiteData();
 const site = enrichSite(rawSite);
 const aCustomer = site.customer;
 // ... 수 많은 코드 ...
 let customer;
-if (aCustomer === "미확인 고객") customerName = "거주자" ;
+if (isUnknown(aCustomer)) customerName = "거주자" ;
 else customerName = aCustomer.name;
 
-function enrichSite(inputSite){
-    return _.cloneDeep(inputSite);
-}
+
 
 // 클라이언트 2
-const plan = (aCustomer === "미확인 고갹") ? 
+const plan = isUnknown(aCustomer) ? 
     registry.billingPlan.basic : aCustomer.billingPlan;
 
-const weeksDelinquent = (aCustomer === "미확인 고갹") ? 
+// 클라이언트 3
+const weeksDelinquent = isUnknown(aCustomer) ? 
     0 : aCustomer.paymentHistory.weeksDelinquentInLastYear;
