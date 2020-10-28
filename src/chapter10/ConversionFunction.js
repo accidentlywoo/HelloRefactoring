707a0773e5a2f4293c72320ac25a269504a34fba
maeve <maeve.woo@goodoc.co.kr>
2020-10-28 19:22:20 +0900

    단순 레코드 구조

---
+++ src/chapter10/ConversionFunction.js
@@ -0,0 +1,23 @@
+JSON jsonBoj =
+{
+    name: "애크미 보스턴",
+    location : "Malden MA",
+    // 더 많은 현장 정보
+    customer: {
+        name: "애크미 산업",
+        billingPlan: "plan-451",
+        paymentHistory: {
+            weeksDelinquentInLastYear: 7
+            // 중략
+        },
+        // 중략
+    }
+};
+
+JSON jsonBoj2 =
+{
+    name: "애크미 보스턴",
+    location : "Malden MA",
+    // 더 많은 현장 정보
+    customer: "미확인 고객",
+};
