81dfe74faa25fd283bbfafb7533680b90284e077
maeve <maeve.woo@goodoc.co.kr>
2020-11-02 22:41:27 +0900

    setter에 어셔선 이동

--- src/chapter10/IntroduceAssertion.js
+++ src/chapter10/IntroduceAssertion.js
@@ -3,9 +3,10 @@ const { assert } = require("chai");
 class Customer {
     applyDiscount(aNumber){
         if(!this.discountRate) return aNumber;
-        else {
-            assert(this.discountRate >= 0);
-            return aNumber - (this.discountRate * aNumber)
-        }
+        else return aNumber - (this.discountRate * aNumber)
+    }
+    set discountRate(aNumber){
+        assert(null === aNumber || aNumber >= 0);
+        this._discountRate = aNumber;
     }
 }
