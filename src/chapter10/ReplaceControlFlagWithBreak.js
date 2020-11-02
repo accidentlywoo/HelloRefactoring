703c2d1fcd8b5ff0a80fbb0d7de91c7122c2f954
maeve <maeve.woo@goodoc.co.kr>
2020-11-02 23:05:25 +0900

    불필요한 제어 플래그 제거

--- src/chapter10/ReplaceControlFlagWithBreak.js
+++ src/chapter10/ReplaceControlFlagWithBreak.js
@@ -1,17 +1,15 @@
 // 생략 중요하지 않은 코드
 checkForMiscreants(people);
-// 생략
+//
 function checkForMiscreants(people){
     for(const p of people){
-        if(!found){
-            if( p === "조커" ){
-                sendAlert();
-                found = true;
-            }
-            if( p === "사루만" ){
-                sendAlert();
-                found = true;
-            }
+        if( p === "조커" ){
+            sendAlert();
+            return;
+        }
+        if( p === "사루만" ){
+            sendAlert();
+            return;
         }
     }
 }
