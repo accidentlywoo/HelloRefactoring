14b415bf645276790bc2b34300ed1d2964ab1cd7
maeve <maeve.woo@goodoc.co.kr>
2020-11-02 23:57:01 +0900

    질의 함수 분리

--- src/chapter11/SeparateQueryFromModifier.js
+++ src/chapter11/SeparateQueryFromModifier.js
@@ -2,12 +2,28 @@ function alertForMiscreant(people){
     for (const p of people){
         if(p === "조커"){
             setOffAlarms();
-            return "조커";
+            return;
         }
         if(p === "사루만"){
             setOffAlarms();
+            return;
+        }
+    }
+    return;
+}
+function findMiscreant(people){
+    for (const p of people){
+        if(p === "조커"){
+            // setOffAlarms();
+            return "조커";
+        }
+        if(p === "사루만"){
+            // setOffAlarms();
             return "사루만";
         }
     }
     return "";
 }
+
+const found = findMiscreant(people);
+alertForMiscreant(people);
