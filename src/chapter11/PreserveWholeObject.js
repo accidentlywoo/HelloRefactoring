9d18d050fb38b79436b0844e8777884aeb396bb7
maeve <maeve.woo@goodoc.co.kr>
2020-11-04 00:24:39 +0900

    실내온도 모니터링 시스템

---
+++ src/chapter11/PreserveWholeObject.js
@@ -0,0 +1,11 @@
+// 실내온도 모니터링 시스템
+class HeatingPlan{
+    withinRange(bottom, top){
+        return (bottom >= this._temperatureRange.low) && top <= (this._temperatureRange.high);
+    }
+}
+
+const low = aRoom.daysTempRange.low;
+const high = aRoom.daysTempRange.high;
+if(!aPlan.withinRange(low, high))
+    alert.push("방 온도가 지정 범위를 벗어났습니다.");
