80a1c8084c3a23bd3283e65fd2850343f5528101
maeve <maeve.woo@goodoc.co.kr>
2020-11-04 20:29:41 +0900

    호출부 변경 및 메서드 이름 변경

--- src/chapter11/ReplaceQueryWithParameter.js
+++ src/chapter11/ReplaceQueryWithParameter.js
@@ -3,7 +3,7 @@ class HeatingPlan {
     get targetTemperature(){
         return this.xxNEWtargetTemperature(thermostat.selectedTemperature);
     }
-    xxNEWtargetTemperature(selectedTemperature){
+    targetTemperature(selectedTemperature){
         if(selectedTemperature > this._max) return this._max;
         else if (selectedTemperature < this._min) return this._min;
         else return selectedTemperature;
@@ -11,6 +11,11 @@ class HeatingPlan {
 }
 
 // 호출자
-if (thePlan.targetTemperature > thermostat.currentTemperature) setToHeat();
-else if (thePlan.targetTemperature < thermostat.currentTemperature) setToCool();
-else setOff();
+if (thePlan.targetTemperature(thermostat.selectedTemperature)
+ > thermostat.currentTemperature) 
+ setToHeat();
+else if (thePlan.targetTemperature(thermostat.selectedTemperature)
+ < thermostat.currentTemperature)
+  setToCool();
+else 
+setOff();
