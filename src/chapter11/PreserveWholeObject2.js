3fb1ff44b5a295eda5a35cdda4a251af19d5416c
maeve <maeve.woo@goodoc.co.kr>
2020-11-04 09:44:31 +0900

    변수 추출하기

--- src/chapter11/PreserveWholeObject2.js
+++ src/chapter11/PreserveWholeObject2.js
@@ -1,4 +1,7 @@
+
+
 const low = aRoom.daysTempRange.low;
 const high = aRoom.daysTempRange.high;
-if(!aPlan.withingRange(low, high))
+const isWithinRange = aPlan.withingRange(low, high);
+if(!isWithinRange)
     alert.push("방 온도가 지정 범위를 벗어났습니다.");
