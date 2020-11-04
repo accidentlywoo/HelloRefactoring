155878cee8fcf324d2eded8bd60dd3343e357dd7
maeve <maeve.woo@goodoc.co.kr>
2020-11-04 09:54:34 +0900

    불필요한 로직 없애기

--- src/chapter11/PreserveWholeObject2.js
+++ src/chapter11/PreserveWholeObject2.js
@@ -1,7 +1,12 @@
 
+function xxNEWwithinRange(aPlan, tempRange){
+    const low = tempRange.low;
+    const high = tempRange.high;
+    const isWithinRange = aPlan.withinRange(low, high);
+    return isWithinRange;
+}
+
 const tempRange = aRoom.daysTempRange;
-const low = tempRange.low;
-const high = tempRange.high;
 const isWithinRange = xxNEWwithinRange(aPlan, tempRange);
 if(!isWithinRange)
     alert.push("방 온도가 지정 범위를 벗어났습니다.");
