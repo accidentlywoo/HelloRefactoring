04b8a2debcc2d46cdf4dec293739f5ae7a9bb20c
maeve <maeve.woo@goodoc.co.kr>
2020-11-04 20:26:56 +0900

    방금 추출한 변수를 인라인

--- src/chapter11/ReplaceQueryWithParameter.js
+++ src/chapter11/ReplaceQueryWithParameter.js
@@ -1,11 +1,7 @@
 // 실내온도 제어 시스템
 class HeatingPlan {
     get targetTemperature(){
-        const selectedTemperature = thermostat.selectedTemperature;
-        return this.xxNEWtargetTemperature(selectedTemperature);
-        // if (selectedTemperature > this._max) return this._max;
-        // else if (selectedTemperature < this._min) return this._min;
-        // else return selectedTemperature;
+        return this.xxNEWtargetTemperature(thermostat.selectedTemperature);
     }
     xxNEWtargetTemperature(selectedTemperature){
         if(selectedTemperature > this._max) return this._max;
