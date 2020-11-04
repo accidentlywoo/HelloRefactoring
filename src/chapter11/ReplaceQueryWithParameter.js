aad474b88abea9167252b523706f3e2930c75ad1
maeve <maeve.woo@goodoc.co.kr>
2020-11-04 20:20:16 +0900

    간단하지만 가추장스러운 실내온도 제어 시스템

---
+++ src/chapter11/ReplaceQueryWithParameter.js
@@ -0,0 +1,13 @@
+// 실내온도 제어 시스템
+class HeatingPlan {
+    get targetTemperature(){
+        if (thermostat.selectedTemperature > this._max) return this._max;
+        else if (thermostat.selectedTemperature < this._min) return this._min;
+        else return thermostat.selectedTemperature;
+    }
+}
+
+// 호출자
+if (thePlan.targetTemperature > thermostat.currentTemperature) setToHeat();
+else if (thePlan.targetTemperature < thermostat.currentTemperature) setToCool();
+else setOff();
