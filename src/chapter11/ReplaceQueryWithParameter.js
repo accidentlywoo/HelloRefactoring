a7225bcf7c5d6e67975158768fd20412ba3f7c93
maeve <maeve.woo@goodoc.co.kr>
2020-11-04 20:25:40 +0900

    메서드 추출하기

--- src/chapter11/ReplaceQueryWithParameter.js
+++ src/chapter11/ReplaceQueryWithParameter.js
@@ -2,7 +2,13 @@
 class HeatingPlan {
     get targetTemperature(){
         const selectedTemperature = thermostat.selectedTemperature;
-        if (selectedTemperature > this._max) return this._max;
+        return this.xxNEWtargetTemperature(selectedTemperature);
+        // if (selectedTemperature > this._max) return this._max;
+        // else if (selectedTemperature < this._min) return this._min;
+        // else return selectedTemperature;
+    }
+    xxNEWtargetTemperature(selectedTemperature){
+        if(selectedTemperature > this._max) return this._max;
         else if (selectedTemperature < this._min) return this._min;
         else return selectedTemperature;
     }
