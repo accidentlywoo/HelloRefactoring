bcbff9078d914aa13e5d5839b3b9138dedd308d0
maeve <maeve.woo@goodoc.co.kr>
2020-11-04 13:42:18 +0900

    주문 클래스

--- src/chapter11/RemoveFlagArgument.js
+++ src/chapter11/RemoveFlagArgument.js
@@ -1,31 +1,15 @@
-function bookConcert(aCustomer, isPremium){
-    if(isPremium){
-        // 프리미엄 예약용 로직
-    }else{
-        // 일반 예약용 로직
+class Order{
+    get finalPrice(){
+        const basePrice = this.quantity * this.itemPrice;
+        let discountLevel;
+        if (this.quantity > 100) discountLevel = 2;
+        else discountLevel = 1;
+        return this.discountedPrice(basePrice, discountLevel);
+    }
+    discountedPrice(basePrice, discountLevel) {
+        switch(discountLevel) {
+            case 1 : return basePrice * 0.95;
+            case 2 : return basePrice * 0.9;
+        }
     }
 }
-
-bookConcert(aCustomer, true);
-
-bookConcert(aCustomer, CustomerType.PREMIUM);
-
-bookConcert(aCustomer, "premium");
-
-function rushDeliveryDate(anOrder){
-    let deliveryTime;
-    if(["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
-    else if(["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else deliveryTime = 3;
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
-}
-
-function regularDeliveryDate(anOrder){
-    let deliveryTime;
-    if(["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else if(["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
-    else deliveryTime = 4;
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
-}
-
-aShipment.deliveryDate = rushDeliveryDate(anOrder);
