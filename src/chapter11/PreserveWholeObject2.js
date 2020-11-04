885baa564f296f5a822ca9965fdf5f88395805a6
maeve <maeve.woo@goodoc.co.kr>
2020-11-04 09:50:48 +0900

    함수 추출하기

--- src/chapter11/PreserveWholeObject2.js
+++ src/chapter11/PreserveWholeObject2.js
@@ -1,7 +1,7 @@
 
-
-const low = aRoom.daysTempRange.low;
-const high = aRoom.daysTempRange.high;
-const isWithinRange = aPlan.withingRange(low, high);
+const tempRange = aRoom.daysTempRange;
+const low = tempRange.low;
+const high = tempRange.high;
+const isWithinRange = xxNEWwithinRange(aPlan, tempRange);
 if(!isWithinRange)
     alert.push("방 온도가 지정 범위를 벗어났습니다.");
