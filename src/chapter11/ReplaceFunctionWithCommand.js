0520f24a0cea1f5e53761f5363c307341e3b22f1
maeve <maeve.woo@goodoc.co.kr>
2020-11-09 00:08:41 +0900

    함수가 유요범위에 구애받지 않고 함수 추출하기같은 리팩터링을 적용할 수 있다

--- src/chapter11/ReplaceFunctionWithCommand.js
+++ src/chapter11/ReplaceFunctionWithCommand.js
@@ -9,10 +9,7 @@ class Scorer{
         this._healthLevel = 0;
         this._highMedicalRiskFlag = false;
 
-        if(this._medicalExam.isSmoker){
-            this._healthLevel += 10;
-            this._highMedicalRiskFlag = true;
-        }
+        this.scoreSmoking();
         this._certificationGrade = "regular";
         if(this._scoringGuide.stateWithLowCertification(this._candidate.originState)){
             this._certificationGrade = "low";
@@ -21,6 +18,12 @@ class Scorer{
         // 비슷한 코드가 한참 이어짐
         this._result -= Math.max(this._healthLevel - 5, 0);
         return this._result;
+    }
+    scoreSmoking(){
+        if(this._medicalExam.isSmoker){
+            this._healthLevel += 10;
+            this._highMedicalRiskFlag = true;
+        }
     }
 }
 
