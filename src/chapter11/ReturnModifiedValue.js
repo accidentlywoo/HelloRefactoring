f977cb3600b78500539f9bd87a44fd8c530f8317
maeve <maeve.woo@goodoc.co.kr>
2020-11-09 23:16:53 +0900

    갱신 사실을 밖으로 알리기

--- src/chapter11/ReturnModifiedValue.js
+++ src/chapter11/ReturnModifiedValue.js
@@ -1,15 +1,16 @@
 // GPS 위치 목록으로 다양한 계산을 수행하는 코드
-function calculateAscent(){
+function calculateAscent(){ // 고도 상승분
     for (let i = 1; i < points.length; i++){
         const verticalChage = points[i].elevation - points[i-1].elevation;
         totalAscent += (verticalChage > 0) ? verticalChage : 0;
     }
+    return totalAscent;
 }
 
 let totalAscent = 0;
 let totalTime = 0;
 let totalDistance = 0;
-calculateAscent();
+totalAscent = calculateAscent();
 calculateTime();
 calculateDistance();
 const pace = totalTime / 60 / totalDistance;
