b2477415fe968a3140907b8a08f0c74f45b2c1ea
maeve <maeve.woo@goodoc.co.kr>
2020-11-09 23:00:13 +0900

    생성자에 전달되는 모든 데이터를 주 메서드로 옮기기 :: 함수 선언 바꾸기

--- src/chapter11/ReplaceCommandWithFunction.js
+++ src/chapter11/ReplaceCommandWithFunction.js
@@ -4,14 +4,14 @@ class ChargeCalculator{
         this._usage = usage;
         this._provider = provider;
     }
-    get charge(){
+    get charge(customer, usage, provider){
         const baseCharge = this._customer.baseCharge * this._usage;
         return baseCharge + this._provider.connectionCharge;
     }
 }
 
 function charge(customer, usage, provider){
-    return new ChargeCalculator(customer, usage, provider).charge;
+    return new ChargeCalculator(customer, usage, provider).charge(customer, usage, provider);
 }
 
 // 호출
