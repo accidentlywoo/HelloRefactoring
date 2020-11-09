1d68da5fbae33888545aca6900f834ab7854cb26
maeve <maeve.woo@goodoc.co.kr>
2020-11-09 23:19:01 +0900

    부모코그에 있는 똑같은 이름의 변수가 가려진다

--- src/chapter11/ReturnModifiedValue.js
+++ src/chapter11/ReturnModifiedValue.js
@@ -1,5 +1,6 @@
 // GPS 위치 목록으로 다양한 계산을 수행하는 코드
 function calculateAscent(){ // 고도 상승분
+    let totalAscent = 0;
     for (let i = 1; i < points.length; i++){
         const verticalChage = points[i].elevation - points[i-1].elevation;
         totalAscent += (verticalChage > 0) ? verticalChage : 0;
@@ -7,7 +8,7 @@ function calculateAscent(){ // 고도 상승분
     return totalAscent;
 }
 
-let totalAscent = 0;
+// let totalAscent = 0;
 let totalTime = 0;
 let totalDistance = 0;
 totalAscent = calculateAscent();
