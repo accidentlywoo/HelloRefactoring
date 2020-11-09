4988657dd5aadb001dbbd1711b10a32231c3c2da
maeve <maeve.woo@goodoc.co.kr>
2020-11-09 23:03:33 +0900

    생성자에서 건네받은 매개변수를 사용하도록 수정

--- src/chapter11/ReplaceCommandWithFunction.js
+++ src/chapter11/ReplaceCommandWithFunction.js
@@ -1,11 +1,11 @@
 class ChargeCalculator{
     constructor(customer, usage, provider){
-        this._customer = customer;
+        // this._customer = customer;
         this._usage = usage;
         this._provider = provider;
     }
-    get charge(customer, usage, provider){
-        const baseCharge = this._customer.baseCharge * this._usage;
+    charge(customer, usage, provider){
+        const baseCharge = customer.baseCharge * this._usage;
         return baseCharge + this._provider.connectionCharge;
     }
 }
