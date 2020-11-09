928d8a9aca76d9056637b7d2e4bb1e46d50a888c
maeve <maeve.woo@goodoc.co.kr>
2020-11-09 23:19:37 +0900

    변수이름은 명명규칙에 맞게 수정

--- src/chapter11/ReturnModifiedValue.js
+++ src/chapter11/ReturnModifiedValue.js
@@ -1,11 +1,11 @@
 // GPS 위치 목록으로 다양한 계산을 수행하는 코드
 function calculateAscent(){ // 고도 상승분
-    let totalAscent = 0;
+    let result = 0;
     for (let i = 1; i < points.length; i++){
         const verticalChage = points[i].elevation - points[i-1].elevation;
-        totalAscent += (verticalChage > 0) ? verticalChage : 0;
+        result += (verticalChage > 0) ? verticalChage : 0;
     }
-    return totalAscent;
+    return result;
 }
 
 // let totalAscent = 0;
