b84d4276c7c80787feeb4d8307626907fbe706f0
maeve <maeve.woo@goodoc.co.kr>
2020-11-16 00:53:40 +0900

    서브 클래스 메서드 제거, 정적언어에서는 추상메서드 구현하자 -> 예시는 오버라이드하지않으면 exception 나게 구현

--- src/chapter12/PullUpMethod.js
+++ src/chapter12/PullUpMethod.js
@@ -1,17 +1,17 @@
 class Party{
+    get monthlyCost(){
+        throw new SubclassResponsibilityError();
+    }
+
     get annualCost(){
         return this.monthlyCost * 12; 
     }
 }
 
 class Employee extends Party{
-    get annualCost(){
-        return this.monthlyCost * 12; 
-    }
+    
 }
 
 class Department extends Party{
-    get annualCost(){
-        return this.monthlyCost * 12; 
-    }
+   
 }
