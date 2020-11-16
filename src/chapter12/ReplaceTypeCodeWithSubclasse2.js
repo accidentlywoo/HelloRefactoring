37d3acfbbec738e1d06f3f3e34204f5cd6146c7b
maeve <maeve.woo@goodoc.co.kr>
2020-11-16 23:54:21 +0900

    직원 유형 리팩터링

--- src/chapter12/ReplaceTypeCodeWithSubclasse2.js
+++ src/chapter12/ReplaceTypeCodeWithSubclasse2.js
@@ -17,11 +17,34 @@ class Employee {
     }
     get typeString(){return this._type.toString();}
     get type(){return this._type;}
-    set type(arg){this._type = arg;}
+    set type(arg){this._type = Employee.createEmployeeType(arg);}
+    static createEmployeeType(aString){
+        switch(aString){
+            case "engineer" : return new Enginner();
+            case "salesperson" : return new Salesperson();
+            case "manager" : return new Manager();
+            default : throw new Error(`${aString}라는 직원 유형은 없습니다.`);
+        }
+    }
 
     get capitalizedType(){
         return this.typeString.charAt(0).toUpperCase() 
         + this.typeString.substr(1).toLowerCase();
     }
     toString() {return `${this._name}(${this.capitalizedType})`;}
+}
+class EmployeeType{
+
+}
+
+class Enginner extends EmployeeType{
+    get type() {return "engieer";}
+}
+
+class Salesperson extends EmployeeType{
+    get type() {return "salesperson";}
+}
+
+class Manager extends EmployeeType{
+    get type() {return "manager";}
 }
