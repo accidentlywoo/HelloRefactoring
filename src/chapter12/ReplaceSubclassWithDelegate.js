66230b047bdfe2be247cbe8df9ca85e6b2a079d8
maeve <maeve.woo@goodoc.co.kr>
2020-11-22 22:43:33 +0900

    오버라이드 메서드 예시

--- src/chapter12/ReplaceSubclassWithDelegate.js
+++ src/chapter12/ReplaceSubclassWithDelegate.js
@@ -3,11 +3,19 @@ class Booking {
         this._show = show;
         this._date = date;
     }
+
+    get hasTalkback(){
+        return this._show.hasOwnProperty('talkback') && !this.isPeakDay;
+    }
 }
 
 class PremiumBooking extends Booking{
     constructor(show, date, extras) {
         super(show, date);
         this._extras = extras;
+    }
+
+    get hasTalkback(){
+        return this._show.hasOwnProperty('talkback');
     }
 }
