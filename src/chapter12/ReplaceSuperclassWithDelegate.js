e6dae16e8e0e874888227e50245804ab852e2394
maeve <maeve.woo@goodoc.co.kr>
2020-11-24 22:19:21 +0900

    스크롤 정기 세척 이력

--- src/chapter12/ReplaceSuperclassWithDelegate.js
+++ src/chapter12/ReplaceSuperclassWithDelegate.js
@@ -8,3 +8,19 @@ class CatalogItem {
     get title() {return this._title;}
     hasTag(arg) {return this._tags.includes(arg);}
 }
+
+class Scroll extends CatalogItem {
+    constructor(id, title, tags, dateLastCleaned){
+        super(id, title, tags);
+        this._lastCleaned = dateLastCleaned;
+    }
+    
+    needsCleaning(targetDate) {
+        const threshold = this.hasTag("revered") ? 700 : 1500;
+        return this.daysSinceLastCleaning(targetDate) > threshold;
+    }
+    
+    daysSinceLastCleaning(targetDate){
+        return this._lastCleaned.until(targetDate, ChronoUnit.DAYS);
+    }
+}
